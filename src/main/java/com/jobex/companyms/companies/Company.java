29006bce290cf710fb1e6830f8ab0ed6c69de57c
Saurav <sauravsynapse@gmail.com>
2024-12-02 16:09:02 +0530

    Initial commit

---
+++ src/main/java/com/jobex/companyms/companies/Company.java
@@ -0,0 +1,84 @@
+package com.jobex.companyms.companies;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.jobex.jobapp.job.Job;
+import com.jobex.jobapp.reviews.Review;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String companyName;
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "company")
+    private List<Job> jobs;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "company")
+    private List<Review> reviews;
+
+    public Company() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public Company(Long id, String companyName, String description, List<Job> jobs) {
+        this.id = id;
+        this.companyName = companyName;
+        this.description = description;
+        this.jobs = jobs;
+    }
+
+    public Long getReviewId() {
+        return id;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
---
+++ src/main/java/com/jobex/companyms/companies/CompanyService.java
@@ -0,0 +1,14 @@
+package com.jobex.companyms.companies;
+import java.util.List;
+
+public interface CompanyService {
+    List<Company> findAll();
+
+    boolean createCompany(Company company);
+
+    Company getCompanyById(Long id);
+
+    boolean updateCompanyById(Long id, Company company);
+
+    boolean deleteCompanyById(Long id);
+}
